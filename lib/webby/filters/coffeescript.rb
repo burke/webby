7bc2656dfd9a2bb4aa3675bb5d84be37f70cb52e
Burke Libbey <burke@burkelibbey.org>
2011-07-19 10:36:38 -0500

    Added coffeescript filter

--- lib/webby/filters/coffeescript.rb
+++ lib/webby/filters/coffeescript.rb
@@ -1,5 +1,13 @@
-require 'coffee-script'
+# Render javascript using CoffeeScript
+if try_require('coffee-script', 'coffee-script')
 
-Webby::Filters.register :coffeescript do |input|
-  CoffeeScript.compile(input)
+  Webby::Filters.register :coffeescript do |input|
+    CoffeeScript.compile(input)
+  end
+
+# Otherwise raise an error if the user tries to use coffeescript
+else
+  Webby::Filters.register :coffeescript do |input|
+    raise Webby::Error, "'coffee-script' must be installed to use the coffeescript filter"
+  end
 end
